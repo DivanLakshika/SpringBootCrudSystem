ef1754b5b6049b84691ce4e74e209e11ec22d9b8
divan lakshika <lakshikadivan@gmail.com>
2023-07-10 12:19:35 +0530

    Commit3

--- src/main/java/com/example/simpleproject/domain/Student.java
+++ src/main/java/com/example/simpleproject/domain/Student.java
@@ -1,14 +1,12 @@
 package com.example.simpleproject.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Student {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+
     private Long id;
     private String studentname;
     private String course;
